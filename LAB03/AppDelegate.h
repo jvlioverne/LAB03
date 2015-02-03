39bdf604f623011c15a3fbc40bf30aa911a2c3ad
jvlioverne <jvlioverne@gmail.com>
2015-02-03 17:09:44 -0600

    Initial Commit

---
+++ LAB03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LAB03
+//
+//  Created by Benjas on 03/02/15.
+//  Copyright (c) 2015 Benjas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LAB03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LAB03
+//
+//  Created by Benjas on 03/02/15.
+//  Copyright (c) 2015 Benjas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
